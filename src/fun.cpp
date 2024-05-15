e396d500a533c9289f2a043242f9a396cf2904ba
TTrunDDun <116082792+TTrunDDun@users.noreply.github.com>
2024-05-15 22:07:47 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,87 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
+#include <math.h>
+#include <cstring>
+#include <stdio.h>
+#include <string>
+#include <cctype>
+#include <sstream>
 
-unsigned int faStr1(const char *str) {
-    return 0;
+using namespace std;
+
+unsigned int faStr1(const char* str) {
+    int Quantity = 0;
+    bool Theword = false;  
+    bool WNumb = false;  
+    
+    for (int i = 1; i < strlen(str); i++) {
+        if (i != 1 && str[i] == ' ' && Theword == true) {
+            if (WNumb == false) {
+                Quantity++;
+            }
+            Theword = false;
+            WNumb = false;
+        }
+        else if (str[i] >= '0' && str[i] <= '9') {
+            WNumb = true;
+            Theword = true;
+        }
+        else if (str[i] != ' ') {
+            if (i == strlen(str) - 1 && WNumb == false) {
+                Quantity++;
+            }
+            Theword = true;
+        }
+    }
+    return Quantity;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr2(const char* str) 
+{
+    string Theword;
+    istringstream String(str);
+    int Quantity = 0;
+
+    while (String >> Theword)
+    {
+        if (isupper(Theword[0]) && !Theword.empty())
+
+        {
+            bool correct = true;
+            for (size_t i = 1; i < Theword.length(); i++) {
+                if (!isspace(Theword[i]) && !islower(Theword[i]))
+                {
+                    correct = false;
+                    break;
+                }
+            }
+
+            if (correct) 
+            {
+                Quantity++;
+            }
+        }
+    }
+
+    return Quantity;
 }
 
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr3(const char* str) 
+{
+    istringstream String(str);
+    string Theword;
+    int ElementsQuantity = 0;
+    float ElementsAvegare = 0;
+
+    while (String >> Theword) 
+    {
+        ElementsAvegare++;
+        ElementsQuantity += Theword.length();   
+    }
+
+    ElementsAvegare = round(ElementsQuantity / ElementsAvegare);
+
+    return ElementsAvegare;
+
 }
