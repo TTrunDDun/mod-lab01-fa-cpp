31a85fcf3a0b4a694e0e847853347d1b8946b656
TTrunDDun <116082792+TTrunDDun@users.noreply.github.com>
2024-05-15 22:32:15 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 #include <iostream>
 #include "fun.h"
 
-using namespace std;
-
 int main() {
     const char* str1 = "qwe12 qwe1 qe qwe qw ";
     const char* str2 = " qweq2 ";
@@ -11,11 +9,11 @@ int main() {
     const char* str4 = "Hello, world!";
     const char* str5 = "hi may";
     const char* str6 = "Hello world";
-    cout << faStr1(str1) << std::endl;
-    cout << faStr1(str2) << std::endl;
-    cout << faStr2(str3) << std::endl;
-    cout << faStr2(str4) << std::endl;
-    cout << faStr3(str5) << std::endl;
-    cout << faStr3(str6) << std::endl;
+    std::cout << faStr1(str1) << std::endl;
+    std::cout << faStr1(str2) << std::endl;
+    std::cout << faStr2(str3) << std::endl;
+    std::cout << faStr2(str4) << std::endl;
+    std::cout << faStr3(str5) << std::endl;
+    std::cout << faStr3(str6) << std::endl;
     return 0;
 }
